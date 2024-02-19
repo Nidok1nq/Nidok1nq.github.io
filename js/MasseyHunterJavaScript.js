1bc5f68e62f9071d9d5af8ec255e7eac62a9f1b3
Nidok1nq <34798245+Nidok1nq@users.noreply.github.com>
2024-02-18 22:13:36 -0500

    Add files via upload

--- js/MasseyHunterJavaScript.js
+++ js/MasseyHunterJavaScript.js
@@ -1,3 +1,15 @@
+var currentView = "About";
+
+function changeSidebar() {
+    var i;
+    var currentSidebars = document.getElementsByClassName("sidebar");
+    for (i = 0; i <currentSidebars.length; i++) {
+        currentSidebars[i].style.color = "rgb(218, 218, 218)";
+    }
+    document.getElementById(currentView + "Sidebar").style.color = "rgb(53, 120, 229)";
+    console.log(currentView + "Sidebar");
+}
+
 function changeView(view) {
     var i;
     var currentViews = document.getElementsByClassName("view")
@@ -5,4 +17,8 @@ function changeView(view) {
         currentViews[i].style.display = "none";
     }
     document.getElementById(view).style.display = "block";
+    currentView = view;
+    changeSidebar();
 }
+
+changeSidebar();
